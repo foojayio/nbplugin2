ae5e137118dc6b8f4ada064072352001ab6bbadd
Emilian Bold <emilian.bold@gmail.com>
2021-01-21 00:05:12 +0200

    Refactors common wizzard boilerplate into AbstractWizardPanel

--- foojaysupport/src/main/java/io/foojay/support/SetupFoojayPlatform.java
+++ foojaysupport/src/main/java/io/foojay/support/SetupFoojayPlatform.java
@@ -1,60 +1,26 @@
 package io.foojay.support;
 
 import io.foojay.api.discoclient.util.PkgInfo;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
-import java.util.ArrayList;
-import java.util.List;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 import org.openide.WizardDescriptor;
-import org.openide.WizardValidationException;
-import org.openide.util.HelpCtx;
 
-public class SetupFoojayPlatform implements WizardDescriptor.AsynchronousValidatingPanel<WizardDescriptor> {
-
-    private FoojayPanel component;
-    private final List<ChangeListener> listeners = new ArrayList<>();
+public class SetupFoojayPlatform extends AbstractWizardPanel<FoojayPanel> {
 
     private final WizardState state;
-    
+
     SetupFoojayPlatform(WizardState state) {
         this.state = state;
     }
 
     @Override
-    public FoojayPanel getComponent() {
-        if (component == null) {
-            component = new FoojayPanel();
-            component.addPropertyChangeListener(FoojayPanel.PROP_DOWNLOAD_SELECTION, new PropertyChangeListener() {
-                @Override
-                public void propertyChange(PropertyChangeEvent e) {
-                    ChangeEvent ce = new ChangeEvent(SetupFoojayPlatform.this);
-                    listeners.forEach(l -> l.stateChanged(ce));
-                }
-            });
-        }
+    public FoojayPanel createComponent() {
+        FoojayPanel component = new FoojayPanel();
+        component.addPropertyChangeListener(FoojayPanel.PROP_DOWNLOAD_SELECTION, (e) -> fireChangeListeners());
         return component;
-    }
-
-    @Override
-    public HelpCtx getHelp() {
-        return HelpCtx.DEFAULT_HELP;
     }
 
     @Override
     public boolean isValid() {
         return getComponent().getBundleInfo() != null;
-    }
-
-    @Override
-    public void addChangeListener(ChangeListener l) {
-        listeners.add(l);
-    }
-
-    @Override
-    public void removeChangeListener(ChangeListener l) {
-        listeners.remove(l);
     }
 
     @Override
@@ -67,14 +33,6 @@ public class SetupFoojayPlatform implements WizardDescriptor.AsynchronousValidat
         wiz.putProperty(FoojayPlatformIt.PROP_FILENAME, bi.getFileName());
         wiz.putProperty(FoojayPlatformIt.PROP_FILEURL, bi.getDirectDownloadUri());
         state.pkgInfo = bi;
-    }
-
-    @Override
-    public void prepareValidation() {
-    }
-
-    @Override
-    public void validate() throws WizardValidationException {
     }
 
 }
